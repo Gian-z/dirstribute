4e0d0ea05d354f16c5960e85baa31b1dc9daa01d
Gian-z <gianluca.zwahlen@cmi.ch>
2025-11-16 16:50:23 +0100

    feat(contracts): Add server interface

---
+++ src/DirstributeContracts/Interfaces/IDirstributeServer.cs
@@ -0,0 +1,18 @@
+﻿namespace DirstributeContracts.Interfaces;
+
+public interface IDirstributeServer
+{
+    Task Map(string name, string source);
+    Task Unmap(string name);
+
+    Task Update(string name);
+    Task UpdateAll();
+
+    Task SetUpdateMode(string name);
+}
+
+public enum UpdateMode
+{
+    OnDemand,
+    OnUpdate
+}
